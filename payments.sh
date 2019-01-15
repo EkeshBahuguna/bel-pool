015a5812f0475077b19479dbb4579208930a7bb5
Ekesh Bahuguna <ekesh.bahuguna@oodlestechnologies.com>
2019-01-15 15:52:36 +0530

    changes done in payments.sh

--- payments.sh
+++
@@ -1,4 +0,0 @@
-echo Sending 172.5 to 6232988412937045830IN
-curl -k -H  "Content-Type: application/json" -X PUT -d '{"publicKey": "358bde6f0084e38291420f40f5d708334ac591b30d4e78ff6e9ce4f6ff8a8a93", "secret": "comic adjust decrease layer tape proof fetch unable frame frozen transfer door", "amount": 1725000000000, "senderCountryCode": "IN", "recepientCountryCode": "IN", "recipientId": "6232988412937045830IN"}' http://localhost:9305/api/transactions
-
-sleep 3
